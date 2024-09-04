58520d434cb24d99ab6778e21355cd6bf372b8a5
ethan shi <ethan.shi@gempoll.com>
2024-09-04 10:31:15 +0000

    首次提交

---
+++ main.py
@@ -0,0 +1,13 @@
+import gradio as gr
+
+
+# 输入文本处理程序
+def greet(name):
+    return name + name
+
+
+# 接口创建函数
+# fn设置处理函数，inputs设置输入接口组件，outputs设置输出接口组件
+# fn,inputs,outputs都是必填函数
+demo = gr.Interface(fn=greet, inputs=gr.Number(), outputs=gr.Number())
+demo.launch()
